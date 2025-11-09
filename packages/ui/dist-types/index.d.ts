931f01d92bc047b76d747a94a9f28cb49aa0efea
srikar-thedarelabs <srikar.panuganti@thedarelabs.com>
2025-11-09 23:13:01 +0530

    v1 commit

---
+++ packages/ui/dist-types/index.d.ts
@@ -0,0 +1,7 @@
+export { cn } from "./lib/utils";
+export { Button, buttonVariants } from "./components/ui/button";
+export { Input } from "./components/ui/input";
+export { Textarea } from "./components/ui/textarea";
+export { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogOverlay, DialogPortal, DialogTitle, DialogTrigger, } from "./components/ui/dialog";
+export { Label } from "./components/ui/label";
+export { Alert, AlertTitle, AlertDescription } from "./components/ui/alert";
